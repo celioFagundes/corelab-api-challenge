94e3ebb2a9314fb169b4ad8f0e441f263f484553
Célio <celiofagundes30@gmail.com>
2022-07-07 09:57:23 -0300

    build: configured postgres database

---
+++ database/factories/VehicleFactory.ts
@@ -0,0 +1,8 @@
+import Vehicle from 'App/Models/Vehicle'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Vehicle, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
