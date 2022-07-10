ca7a2d30bd2c32c1c83d7125367e450aadd9d153
Célio <celiofagundes30@gmail.com>
2022-07-10 19:32:30 -0300

    fix: deploy

--- database/factories/VehicleFactory.ts
+++ database/factories/VehicleFactory.ts
@@ -1,7 +1,7 @@
 import Vehicle from 'App/Models/Vehicle'
 import Factory from '@ioc:Adonis/Lucid/Factory'
 
-export default Factory.define(Vehicle, ({ faker }) => {
+export default Factory.define(Vehicle, () => {
   return {
     //
   }
